a1727359c8584b610210ed9b4a9bd20c0bfec05f
yutuer <yutuerzf@163.com>
2017-05-24 21:43:13 +0800

    32

--- src/main/java/yutuer/AgentLaunch.java
+++ src/main/java/yutuer/AgentLaunch.java
@@ -1,16 +1,23 @@
 package yutuer;
 
 import java.lang.instrument.Instrumentation;
+
+import org.apache.log4j.Logger;
+
 import yutuer.transfer.PrintClassFileTransformer;
 
 public class AgentLaunch {
 
+	private static Logger log = Logger.getLogger(AgentLaunch.class);
+	
 	public static void premain(String args, Instrumentation inst) {
         main(args, inst);
+        log.info(String.format("premain success, args:%s, inst:%s", args, inst));
     }
 
     public static void agentmain(String args, Instrumentation inst) {
         main(args, inst);
+        log.info(String.format("agentmain success, args:%s, inst:%s", args, inst));
     }
 
 	private static void main(String args, Instrumentation inst) {
